30250559f2f9f6371affcde2db016368a1241fff
AlbGj <47143881+AlbGj@users.noreply.github.com>
2024-06-17 01:08:47 -0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    alert('Welcome to AI Innovate!');
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     alert('Welcome to AI Innovate!');
+// });
